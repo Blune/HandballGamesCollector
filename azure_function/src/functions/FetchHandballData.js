5655fb0ce1e39f422c087808e9bddce952d8eee7
Matthias Abele <mabele@artiso.com>
2023-10-19 21:15:02 +0200

    team names and more properties

--- azure_function/src/functions/FetchHandballData.js
+++ azure_function/src/functions/FetchHandballData.js
@@ -3,32 +3,17 @@ const { BlobServiceClient } = require("@azure/storage-blob");
 const fetch = require('node-fetch');
 
 app.timer('FetchHandballData', {
-    schedule: '0 44 4 * * 1',
+    schedule: '0 00 23 * * 4',
     handler: async (myTimer, context) => {
         context.log(`Starting scheduled function`);
 
+        const city = "Laichingen"
         const now = new Date();
-        const mondayDate = new Date(now);
-        mondayDate.setDate(now.getDate() - (now.getDay() + 6) % 7); // Go back to Monday
-        const mondayOfThisWeek = mondayDate.toISOString().split('T')[0];
+        const mondayOfThisWeek = getDateOfMonday(now);
+        let allDates = await getMatchDates(mondayOfThisWeek);
+        const allGames = await getAllGames(allDates);
 
-        const url = 'https://spo.handball4all.de/service/if_g_json.php?'
-        const orgBodenseeDonau = "12";
-        const baWue = "3";
-        const query = "do=" + mondayOfThisWeek + "&cmd=po&og=3&o=" + orgBodenseeDonau
-        const team = "TSV Laichingen";
-
-        let allDates = await fetch(url + query)
-            .then(response => { return response.json() })
-            .then(json => { return json[0].menu.dt.list })
-            .then(dates => Object.getOwnPropertyNames(dates));
-
-        context.log(`all dates fetched: "${allDates.length}"`);
-
-        const urls = allDates.map(date => `${url}do=${date}&cmd=po&og=${baWue}&o=${orgBodenseeDonau}`);
-        const requests = urls.map((url) => fetch(url).then(response => response.json()));
-        const allGames = await Promise.all(requests);
-
+        const teamLA = `TSV ${city}`
         let gamesOfTeam = allGames
             .flatMap(json => json[0].content.classes)
             .filter(c => c.games.length > 0)
@@ -37,55 +22,131 @@ app.timer('FetchHandballData', {
                 return c;
             })
             .flatMap(c => c.games)
-            .filter(game => game.gHomeTeam.startsWith(team) || game.gGuestTeam.startsWith(team))
+            .filter(game => game.gHomeTeam.startsWith(teamLA) || game.gGuestTeam.startsWith(teamLA))
             .map(element => {
-                const dateComponents = element.gDate.split('.');
-                const day = parseInt(dateComponents[0], 10);
-                const month = parseInt(dateComponents[1], 10);
-                const monthIndex = month-1;
-                const year = parseInt("20" + dateComponents[2], 10);
+                const teamNames = mapTeamName(element.team);
+                const homeGame = element.gHomeTeam.startsWith(teamLA);
+                const dateTime = getDateTime(element.gDate, element.gTime);
                 return {
                     team: element.team,
-                    home: element.gHomeTeam.startsWith(team) ? element.gHomeTeam : element.gGuestTeam,
-                    opponent: element.gHomeTeam.startsWith(team) ? element.gGuestTeam : element.gHomeTeam,
-                    goals: element.gHomeTeam.startsWith(team) ? element.gHomeGoals : element.gHomeGoals_1,
-                    opponentgoals: element.gHomeTeam.startsWith(team) ? element.gHomeGoals_1 : element.gHomeGoals,
+                    teamname: teamNames.long,
+                    shortteamname: teamNames.short,
+                    upperteamname: teamNames.upper,
+                    home: homeGame ? element.gHomeTeam : element.gGuestTeam,
+                    opponent: homeGame ? element.gGuestTeam : element.gHomeTeam,
+                    goals: homeGame ? element.gHomeGoals : element.gHomeGoals_1,
+                    opponentgoals: homeGame ? element.gHomeGoals_1 : element.gHomeGoals,
                     date: element.gDate,
-                    dateCompare: new Date(year, monthIndex, day),
+                    dateCompare: dateTime,
                     day: element.gWDay,
                     time: element.gTime,
+                    matchtype: element.gGymnasiumTown == city ? "H" : "A",
                     match: `${element.gHomeTeam} vs ${element.gGuestTeam}`,
                     place: `in ${element.gGymnasiumTown}`
                 };
             });
 
-        const in10Days = new Date(now);
-        in10Days.setDate(now.getDate() + 10);
-        const nextGames = gamesOfTeam
-            .filter(game => game.dateCompare >= now && game.dateCompare <= in10Days )
-            .sort((a, b) => new Date(a.dateCompare) - new Date(b.dateCompare))
-            .reduce((accumulator, match) => {
-                const team = match.team;
-                if (!accumulator.has(team) || new Date(match.dateCompare) > new Date(accumulator.get(team).dateCompare)) {
-                  accumulator.set(team, match);
-                }
-                return accumulator;
-              }, new Map());
+        const nextMatchesArray = getNextMatches(now, gamesOfTeam);
 
-        const nextMatchesArray = Array.from(nextGames.values());
-        
-        const connectionString = process.env["AzureWebJobsStorage"];
-        const containerName = process.env["STORAGE_CONTAINER_NAME"];
-        const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
-        const containerClient = blobServiceClient.getContainerClient(containerName);
-        const blobClient = containerClient.getBlockBlobClient('allgames.json');
-        await blobClient.upload(JSON.stringify(gamesOfTeam), JSON.stringify(gamesOfTeam).length);
+        const containerClient = getContainerClient();
+        storeInContainer(containerClient, gamesOfTeam, 'allgames.json');
         context.log('all games stored successfully.');
-
-        const nextblobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
-        const nextcontainerClient = nextblobServiceClient.getContainerClient(containerName);
-        const nextblobClient = nextcontainerClient.getBlockBlobClient('nextgames.json');
-        await nextblobClient.upload(JSON.stringify(nextMatchesArray), JSON.stringify(nextMatchesArray).length);
+        storeInContainer(containerClient, nextMatchesArray, 'nextgames.json');
         context.log('next games stored successfully.');
     }
 });
+
+async function getAllGames(allDates) {
+    const urls = allDates.map(date => getQuery(date));
+    const requests = urls.map((url) => fetch(url).then(response => response.json()));
+    const allGames = await Promise.all(requests);
+    return allGames;
+}
+
+async function getMatchDates(mondayOfThisWeek) {
+    return await fetch(getQuery(mondayOfThisWeek))
+        .then(response => { return response.json(); })
+        .then(json => { return json[0].menu.dt.list; })
+        .then(dates => Object.getOwnPropertyNames(dates));
+}
+
+function getQuery(date) {
+    const url = 'https://spo.handball4all.de/service/if_g_json.php?';
+    const orgBodenseeDonau = "12";
+    const baWue = "3";
+    return `${url}do=${date}&cmd=po&og=${baWue}&o=${orgBodenseeDonau}`;
+}
+
+function getDateOfMonday(now) {
+    const mondayDate = new Date(now);
+    mondayDate.setDate(now.getDate() - (now.getDay() + 6) % 7); // Go back to Monday
+    const mondayOfThisWeek = mondayDate.toISOString().split('T')[0];
+    return mondayOfThisWeek;
+}
+
+function mapTeamName(team) {
+    switch (team) {
+        case 'M-KLA-D':
+            return { short: 'M1', long: 'Männer 1', upper: 'MÄNNER 1' };
+        case 'M-KLB-D':
+            return { short: 'M2', long: 'Männer 2', upper: 'MÄNNER 2' };
+        case 'F-KLA-D':
+            return { short: 'D1', long: 'Damen 1', upper: 'DAMEN 1' };
+        case 'F-KLB-D':
+            return { short: 'D2', long: 'Damen 2', upper: 'DAMEN 2' };
+        case 'gJD-KLC-1':
+            return { short: 'gD', long: 'Gemischte D-Jugend', upper: 'gD-JUGEND' };
+        case 'wJB-KL-1':
+            return { short: 'wB', long: 'Weibliche B-Jugend', upper: 'wB-JUGEND' };
+        case 'gJE-6+1':
+            return { short: 'gE', long: 'Gemischte E-Jugend', upper: 'gE-JUGEND' };
+        case 'mJB-KL-1':
+            return { short: 'mB', long: 'Männliche B-Jugend', upper: 'mB-JUGEND' };
+        case 'mJC-KLC-1':
+            return { short: 'mC', long: 'Männliche C-Jugend', upper: 'mC-JUGEND' };
+        default:
+            return { short: '', long: team, upper: team };
+    }
+}
+
+function getDateTime(date, time) {
+    const dateComponents = date.split('.');
+    const day = parseInt(dateComponents[0], 10);
+    const month = parseInt(dateComponents[1], 10);
+    const monthIndex = month - 1;
+    const year = parseInt("20" + dateComponents[2], 10);
+    const timeComponents = time.split(':');
+    const hours = parseInt(timeComponents[0], 10);
+    const minutes = parseInt(timeComponents[1], 10);
+    return new Date(year, monthIndex, day, hours, minutes);
+}
+
+function getNextMatches(now, gamesOfTeam) {
+    const in10Days = new Date(now);
+    in10Days.setDate(now.getDate() + 10);
+    const nextGames = gamesOfTeam
+        .filter(game => game.dateCompare >= now && game.dateCompare <= in10Days)
+        .reduce((accumulator, match) => {
+            const id = `${match.team}-${match.home}`;
+            if (!accumulator.has(id) || new Date(match.dateCompare) > new Date(accumulator.get(id).dateCompare)) {
+                accumulator.set(id, match);
+            }
+            return accumulator;
+        }, new Map());
+
+    const nextMatchesArray = Array.from(nextGames.values())
+        .sort((a, b) => new Date(a.dateCompare) - new Date(b.dateCompare));
+    return nextMatchesArray;
+}
+
+function storeInContainer(containerClient, data, name) {
+    containerClient
+        .getBlockBlobClient(name)
+        .upload(JSON.stringify(data), JSON.stringify(data).length);
+}
+
+function getContainerClient() {
+    return BlobServiceClient
+        .fromConnectionString(process.env["AzureWebJobsStorage"])
+        .getContainerClient(process.env["STORAGE_CONTAINER_NAME"]);
+}
