e2462db9f938e884f4f59567c5fc7a77fb42dee9
Matthias Abele <mabele@artiso.com>
2023-12-21 17:25:52 +0100

    set md5 hash

--- terraform/packaging.tf
+++ terraform/packaging.tf
@@ -23,3 +23,12 @@ data "archive_file" "function_zip" {
 
   depends_on = [null_resource.create_function_package]
 }
+
+resource "azurerm_storage_blob" "storage_blob_function" {
+  name                   = "function.zip"
+  storage_account_name   = azurerm_storage_account.handball-storage-account.name
+  storage_container_name = azurerm_storage_container.handball-deployments-storage-container.name
+  type                   = "Block"
+  source                 = data.archive_file.function_zip.output_path
+  content_md5            = data.archive_file.function_zip.output_md5
+}
--- terraform/storage_account.tf
+++ terraform/storage_account.tf
@@ -37,11 +37,5 @@ resource "azurerm_storage_container" "handball-deployments-storage-container" {
   container_access_type = "private"
 }
 
-resource "azurerm_storage_blob" "storage_blob_function" {
-  name                   = "function.zip"
-  storage_account_name   = azurerm_storage_account.handball-storage-account.name
-  storage_container_name = azurerm_storage_container.handball-deployments-storage-container.name
-  type                   = "Block"
-  source                 = data.archive_file.function_zip.output_path
-}
+
 
