1fc04c7036ff8e8e2a1dc6dfe26be326a437f76b
Matthias Abele <mabele@artiso.com>
2024-01-12 23:34:16 +0100

    typo

--- terraform/website.tf
+++ terraform/website.tf
@@ -8,8 +8,8 @@ locals {
   }
   prefix = "https://${azurerm_storage_account.handball-storage-account.name}.blob.core.windows.net/${azurerm_storage_container.handball-storage-container.name}"
   urls = <<EOT
-const allGamesUrl = "${locals.prefix}/allgames.json${data.azurerm_storage_account_blob_container_sas.function_results_sas.sas}"
-const nextGamesUrl = "${locals.prefix}/nextgames.json${data.azurerm_storage_account_blob_container_sas.function_results_sas.sas}"
+const allGamesUrl = "${local.prefix}/allgames.json${data.azurerm_storage_account_blob_container_sas.function_results_sas.sas}"
+const nextGamesUrl = "${local.prefix}/nextgames.json${data.azurerm_storage_account_blob_container_sas.function_results_sas.sas}"
 EOT 
 }
 
