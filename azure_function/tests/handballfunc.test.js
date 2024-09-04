c77dd6f324f978c3e8d091f17b7ee92a8bbde888
Blune <blune@web.de>
2024-09-04 20:23:12 +0200

    Update handballfunc.test.js

--- azure_function/tests/handballfunc.test.js
+++ azure_function/tests/handballfunc.test.js
@@ -103,27 +103,6 @@ describe('fetchAndStoreGames', () => {
   });
 });
 
-describe('mapTeamName', () => {
-  test('should return the correct team name', async () => {
-    expect(handball.mapTeamName("M-KLA-D", ""))
-      .toStrictEqual({ short: 'M1', long: 'Männer 1', upper: 'MÄNNER 1' });
-  });
-});
-
-describe('mapTeamName', () => {
-  test('should return the correct team name for the women team 1', async () => {
-    expect(handball.mapTeamName("F-KLA-D", "TSV Laichingen"))
-      .toStrictEqual({ short: 'D1', long: 'Damen 1', upper: 'DAMEN 1' });
-  });
-});
-
-describe('mapTeamName', () => {
-  test('should return the correct team name for the women team 2', async () => {
-    expect(handball.mapTeamName("F-KLA-D", "TSV Laichingen 2"))
-      .toStrictEqual({ short: 'D2', long: 'Damen 2', upper: 'DAMEN 2' });
-  });
-});
-
 describe('getDateOfMonday', () => {
   test('should return the date of monday of the current week', async () => {
     expect(handball.getDateOfMonday(new Date(2023, 10, 11)))
