42cc0c9a3adbabe0b0ed4d03a100fc4b4ceb636e
Blune <blune@web.de>
2024-09-21 15:48:51 +0200

    disable function for recreation

--- terraform/function.tf
+++ terraform/function.tf
@@ -1,48 +1,48 @@
-resource "azurerm_linux_function_app" "handball-linux-function-app" {
-  depends_on = [azurerm_storage_account.handball-storage-account, azurerm_storage_container.handball-storage-container, azurerm_storage_blob.storage_blob_function, data.azurerm_storage_account_blob_container_sas.sas]
+# resource "azurerm_linux_function_app" "handball-linux-function-app" {
+#   depends_on = [azurerm_storage_account.handball-storage-account, azurerm_storage_container.handball-storage-container, azurerm_storage_blob.storage_blob_function, data.azurerm_storage_account_blob_container_sas.sas]
 
-  name                = "${local.name}-linux-function-app"
-  resource_group_name = azurerm_resource_group.handball-resource-group.name
-  location            = azurerm_resource_group.handball-resource-group.location
+#   name                = "${local.name}-linux-function-app"
+#   resource_group_name = azurerm_resource_group.handball-resource-group.name
+#   location            = azurerm_resource_group.handball-resource-group.location
 
-  storage_account_name       = azurerm_storage_account.handball-storage-account.name
-  storage_account_access_key = azurerm_storage_account.handball-storage-account.primary_access_key
-  service_plan_id            = azurerm_service_plan.handball-app-service-plan.id
+#   storage_account_name       = azurerm_storage_account.handball-storage-account.name
+#   storage_account_access_key = azurerm_storage_account.handball-storage-account.primary_access_key
+#   service_plan_id            = azurerm_service_plan.handball-app-service-plan.id
 
-  https_only = true
+#   https_only = true
 
-  app_settings = {
-    FUNCTIONS_WORKER_RUNTIME       = "node"
-    WEBSITE_RUN_FROM_PACKAGE       = "https://${azurerm_storage_account.handball-storage-account.name}.blob.core.windows.net/${azurerm_storage_container.handball-deployments-storage-container.name}/${azurerm_storage_blob.storage_blob_function.name}${data.azurerm_storage_account_blob_container_sas.sas.sas}"
-    APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_application_insights.application_insights.instrumentation_key}"
-    STORAGE_CONTAINER_NAME         = "${azurerm_storage_container.handball-storage-container.name}"
-    # WEBSITE_TIME_ZONE              = "W. Europe Standard Time" #Not supported
-    QUERY_URL               = "https://spo.handball4all.de/service/if_g_json.php?"
-    TEAM_CITY               = "Laichingen"
-    TEAM_REGION_BAWUE       = "3"
-    TEAM_ORG_BODENSEE_DONAU = "12"
-  }
+#   app_settings = {
+#     FUNCTIONS_WORKER_RUNTIME       = "node"
+#     WEBSITE_RUN_FROM_PACKAGE       = "https://${azurerm_storage_account.handball-storage-account.name}.blob.core.windows.net/${azurerm_storage_container.handball-deployments-storage-container.name}/${azurerm_storage_blob.storage_blob_function.name}${data.azurerm_storage_account_blob_container_sas.sas.sas}"
+#     APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_application_insights.application_insights.instrumentation_key}"
+#     STORAGE_CONTAINER_NAME         = "${azurerm_storage_container.handball-storage-container.name}"
+#     # WEBSITE_TIME_ZONE              = "W. Europe Standard Time" #Not supported
+#     QUERY_URL               = "https://spo.handball4all.de/service/if_g_json.php?"
+#     TEAM_CITY               = "Laichingen"
+#     TEAM_REGION_BAWUE       = "3"
+#     TEAM_ORG_BODENSEE_DONAU = "12"
+#   }
 
-  site_config {
-    application_stack {
-      node_version = local.node_version
-    }
-  }
-}
+#   site_config {
+#     application_stack {
+#       node_version = local.node_version
+#     }
+#   }
+# }
 
-data "azurerm_storage_account_blob_container_sas" "sas" {
-  connection_string = azurerm_storage_account.handball-storage-account.primary_connection_string
-  container_name    = azurerm_storage_container.handball-deployments-storage-container.name
+# data "azurerm_storage_account_blob_container_sas" "sas" {
+#   connection_string = azurerm_storage_account.handball-storage-account.primary_connection_string
+#   container_name    = azurerm_storage_container.handball-deployments-storage-container.name
 
-  start  = local.current_date
-  expiry = local.two_years_later_date
+#   start  = local.current_date
+#   expiry = local.two_years_later_date
 
-  permissions {
-    read   = true
-    add    = false
-    create = false
-    write  = false
-    delete = false
-    list   = false
-  }
-}
+#   permissions {
+#     read   = true
+#     add    = false
+#     create = false
+#     write  = false
+#     delete = false
+#     list   = false
+#   }
+# }
