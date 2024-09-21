f9e26c9ee29b12f8ffd72d5e74b763147d97cec6
Blune <blune@web.de>
2024-09-21 16:08:24 +0200

    test redeploy

--- terraform/packaging.tf
+++ terraform/packaging.tf
@@ -24,17 +24,17 @@ data "archive_file" "function_zip" {
   source_dir  = "${local.functionPath}/"
 }
 
-# resource "azurerm_storage_blob" "storage_blob_function" {
-#   depends_on = [data.archive_file.function_zip]
+resource "azurerm_storage_blob" "storage_blob_function" {
+  depends_on = [data.archive_file.function_zip]
 
-#   name                   = local.zipfile
-#   storage_account_name   = azurerm_storage_account.handball-storage-account.name
-#   storage_container_name = azurerm_storage_container.handball-deployments-storage-container.name
-#   type                   = "Block"
-#   source                 = data.archive_file.function_zip.output_path
-#   # content_md5            = data.archive_file.function_zip.output_md5
-#   # metadata = {
-#   #   # Force re-deploy if hash changes
-#   #   package_hash = data.archive_file.function_zip.output_base64sha256
-#   # }
-# }
+  name                   = local.zipfile
+  storage_account_name   = azurerm_storage_account.handball-storage-account.name
+  storage_container_name = azurerm_storage_container.handball-deployments-storage-container.name
+  type                   = "Block"
+  source                 = data.archive_file.function_zip.output_path
+  content_md5            = data.archive_file.function_zip.output_md5
+  metadata = {
+    # Force re-deploy if hash changes
+    package_hash = data.archive_file.function_zip.output_base64sha256
+  }
+}
